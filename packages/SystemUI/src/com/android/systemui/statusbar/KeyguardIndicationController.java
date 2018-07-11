654f3b99441d223ee3f08d37c3bc402965a417e7
AgustinLorenzo <agustin.lorenzo@thinco.es>
2018-07-11 22:02:25 +0000

    Recodificado en UTF-8 y corregido un simbolo

--- packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
@@ -377,12 +377,12 @@ public class KeyguardIndicationController {
                 batteryInfo = batteryInfo + (mChargingCurrent / 1000) + "mA";
             }
             if (mChargingVoltage > 0) {
-                batteryInfo = (batteryInfo == "" ? "" : batteryInfo + " . ") +
+                batteryInfo = (batteryInfo == "" ? "" : batteryInfo + " · ") +
                         String.format("%.1f", (mChargingVoltage / 1000 / 1000)) + "V";
             }
             if (mTemperature > 0) {
-                batteryInfo = (batteryInfo == "" ? "" : batteryInfo + " . ") +
-                        mTemperature / 10 + "�C";
+                batteryInfo = (batteryInfo == "" ? "" : batteryInfo + " · ") +
+                        mTemperature / 10 + "°C";
             }
             if (batteryInfo != "") {
                 batteryInfo = "\n" + batteryInfo;
